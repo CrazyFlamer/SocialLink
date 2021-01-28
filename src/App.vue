f2f1527a14b29a5402a9bea9eb8942b602b79d5a
CrazyFlamer <CrazyFlamer@github.com>
2021-01-28 20:10:49 +0300

    first and second step

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,102 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+   <v-app id="app">
+      <v-navigation-drawer
+      color="green"
+      dark
+      expand-on-hover
+      hide-overlay
+      permanent
+      right
+      app
+      >
+        <v-list
+            nav 
+            shaped
+            dense
+        >
+          <v-list-item>
+            <v-list-item-avatar>
+              <img :src="'https://randomuser.me/api/portraits/men/1.jpg'">
+            </v-list-item-avatar>
+
+            <v-list-item-content class="text-left">
+
+              <v-list-item-title class="font-weight-black">
+                  SocialLink
+              </v-list-item-title>
+              <v-list-item-subtitle>
+                Прикольный Парень
+              </v-list-item-subtitle>
+
+            </v-list-item-content>
+          </v-list-item>
+
+          <v-divider class="my-3"></v-divider>
+
+          <v-list-item link to="">
+
+            <v-list-item-icon>
+              <v-icon>mdi-freebsd</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title class ="text-left">Boss-page</v-list-item-title>
+            </v-list-item-content>
+
+          </v-list-item>
+
+          <v-list-item link to="">
+
+            <v-list-item-icon>
+              <v-icon>mdi-unicorn-variant</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title class ="text-left">Virtual-You</v-list-item-title>
+            </v-list-item-content>
+
+          </v-list-item>
+
+          <v-list-item link to="">
+
+            <v-list-item-icon>
+              <v-icon>mdi-sunglasses</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title class ="text-left">Virtual-Your-Friends</v-list-item-title>
+            </v-list-item-content>
+
+          </v-list-item>
+
+        </v-list>
+      </v-navigation-drawer>
+    
+    <v-content class="px-12 py-3">
+      <v-container fluid>
+        <router-view/>
+      </v-container>
+    </v-content>
+
+  </v-app>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
 }
 </style>
